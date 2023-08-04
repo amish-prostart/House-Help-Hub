93dda72040c91adbd6ef493a96c1752e9c635a8d
amishyadav <Amishyadav6@gmail.com>
2023-08-04 08:29:47 +0530

    feat: add tabler theme and yajra package

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
+class CategoryController extends Controller
+{
+    /**
+     * @throws \Exception
+     */
+    public function index(Request $request)
+    {
+
+        if ($request->ajax()) {
+
+            $data = Category::all();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function($row){
+
+                    $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
+
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+        return view('categories.index');
+    }
+}
